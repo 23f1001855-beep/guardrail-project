114b73f7591be41b3e4065c8412721355ca32477
Aditya Nalavade <adityanalavade@MacBook-Air.local>
2026-07-23 21:49:49 +0530

    Use Render PORT

--- app.py
+++ app.py
@@ -154,3 +154,11 @@ if __name__ == "__main__":
         port=5001,
         debug=True
     )
+import os
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5001)),
+        debug=False
+    )
